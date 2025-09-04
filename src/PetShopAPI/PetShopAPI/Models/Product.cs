1a7d9e80f8e86e801543b2bdbb89568ce2810cd0
JuliaStah <149059675+JuliaStah@users.noreply.github.com>
2025-09-04 14:05:19 +0300

    Добавление файлов проекта

---
+++ src/PetShopAPI/PetShopAPI/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace PetShopAPI.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Category { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+    }
+}
